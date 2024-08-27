1ee2222e2770cd61ec52b908076d2d95d54896be
yathuchandran <yathuchandran67437@gmail.com>
2024-08-27 10:35:34 +0530

    first commit

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -55,13 +55,14 @@ export const VerifyOtp = async (otp,Mobile) => {
 
 
 
-export const ForgotPassword = async (otp,Mobile) => {
+export const ForgotPswrd = async (Mobile) => {
+  console.log(Mobile,"Mobile=================================");
   const formData = new FormData();
-  formData.append("otp", otp);
   formData.append("mobile", Mobile);
   formData.append("X-Api-Key", ApiKey);  
+
   try {
-    const response = await axios.post( `${baseUrlApi}login_otp/update/`,formData, {
+    const response = await axios.post( `${baseUrlApi}login_otp/add/`,formData, {
         headers: {
           "X-Api-Key": "8YUI3673DEB6F281A8F2E856902HJKU7",
           "Content-Type": "multipart/form-data",
--- src/Components/Login/ForgetPswrd.jsx
+++ src/Components/Login/ForgetPswrd.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Paper, Grid, Box, Typography, TextField, Button, IconButton, InputAdornment, useMediaQuery, Container } from "@mui/material";
+import {
+  Paper,
+  Grid,
+  Box,
+  Typography,
+  TextField,
+  Button,
+  IconButton,
+  InputAdornment,
+  useMediaQuery,
+  Container,
+} from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { blue } from "@mui/material/colors";
 import { useNavigate } from "react-router-dom";
 import { secondaryColorTheme } from "../../config";
+import { ForgotPswrd } from "../../Api/Api";
 
 const defaultTheme = createTheme();
 
@@ -24,22 +36,29 @@ const ForgotPassword = () => {
 
   const isSmallScreen = useMediaQuery(defaultTheme.breakpoints.down("sm"));
 
-  const handlePhoneNumberChange = (e) => {setPhoneNumber(e.target.value);
-    console.log(e.target.value ,"handlePhoneNumberChange");
-  }
-  const handleSubmitNum=()=>{
-
-    const res=
-    console.log("button click akkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
-  }
-  const handleOtpChange = (e) => {setOtp(e.target.value);
-    console.log(e.target.value ,"handleOtpChange");
-  }
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+    console.log(e.target.value, "handlePhoneNumberChange");
+  };
+  const handleSubmitNum = async () => {
+    try {
+      
+      const res = await ForgotPswrd(phoneNumber);
+      console.log(res,phoneNumber,"button click akkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleOtpChange = (e) => {
+    setOtp(e.target.value);
+    console.log(e.target.value, "handleOtpChange");
+  };
   const handleNewPasswordChange = (e) => setNewPassword(e.target.value);
   const handleConfirmPasswordChange = (e) => setConfirmPassword(e.target.value);
 
-//   console.log(phoneNumber, "phoneNumber");
-  
+  //   console.log(phoneNumber, "phoneNumber");
+
   const handleSendOtp = (e) => {
     e.preventDefault();
     const message = `OTP has been sent to ${phoneNumber}`;
@@ -50,7 +69,8 @@ const ForgotPassword = () => {
 
   const handleVerifyOtp = (e) => {
     e.preventDefault();
-    if (otp === "123456") { // Dummy OTP check
+    if (otp === "123456") {
+      // Dummy OTP check
       setVerificationSuccess(true);
     } else {
       alert("Invalid OTP. Please try again.");
@@ -67,8 +87,10 @@ const ForgotPassword = () => {
     }
   };
 
-  const toggleNewPasswordVisibility = () => setShowNewPassword(!showNewPassword);
-  const toggleConfirmPasswordVisibility = () => setShowConfirmPassword(!showConfirmPassword);
+  const toggleNewPasswordVisibility = () =>
+    setShowNewPassword(!showNewPassword);
+  const toggleConfirmPasswordVisibility = () =>
+    setShowConfirmPassword(!showConfirmPassword);
 
   useEffect(() => {
     if (otpExpiration > 0) {
@@ -80,36 +102,77 @@ const ForgotPassword = () => {
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
-    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid 
-       container
-       component="main"
-       justifyContent="center"
-       alignItems="center"
-       sx={{
-         backgroundColor: secondaryColorTheme,
-         height: '94vh',
-         width: '99vw',
-         padding: '16px',
-       }}
-        >
+      <Grid
+        container
+        component="main"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          backgroundColor: secondaryColorTheme,
+          height: "94vh",
+          width: "99vw",
+          padding: "16px",
+        }}
+      >
         <Container maxWidth="sm" sx={{ mt: 4 }}>
-          <Box sx={{ backgroundColor: "#f5f5f5", padding: isSmallScreen ? 2 : 4, borderRadius: 2, width: '100%', maxWidth: 400, mx: 'auto' , boxShadow: 3, }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Box
+            sx={{
+              backgroundColor: "#f5f5f5",
+              padding: isSmallScreen ? 2 : 4,
+              borderRadius: 2,
+              width: "100%",
+              maxWidth: 400,
+              mx: "auto",
+              boxShadow: 3,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
               <Typography component="h1" variant="h5" textAlign="center">
-                <b>{passwordReset ? "Password Reset Successful" : (verificationSuccess ? "Set New Password" : otpSent ? "Verify OTP" : "Forgot Password")}</b>
+                <b>
+                  {passwordReset
+                    ? "Password Reset Successful"
+                    : verificationSuccess
+                    ? "Set New Password"
+                    : otpSent
+                    ? "Verify OTP"
+                    : "Forgot Password"}
+                </b>
               </Typography>
-              <Box component="form" onSubmit={otpSent ? (verificationSuccess ? handleResetPassword : handleVerifyOtp) : handleSendOtp} sx={{ mt: 3 }}>
+              <Box
+                component="form"
+                onSubmit={
+                  otpSent
+                    ? verificationSuccess
+                      ? handleResetPassword
+                      : handleVerifyOtp
+                    : handleSendOtp
+                }
+                sx={{ mt: 3 }}
+              >
                 {!verificationSuccess ? (
                   <>
                     {!otpSent ? (
                       <Box>
-                        <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
-                          <b>Enter your registered mobile number below to receive password reset instructions</b>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          sx={{ mb: 2 }}
+                        >
+                          <b>
+                            Enter your registered mobile number below to receive
+                            password reset instructions
+                          </b>
                         </Typography>
                         <TextField
                           fullWidth
@@ -122,11 +185,22 @@ const ForgotPassword = () => {
                           autoFocus
                         />
                         {otpMessage && ( // Display the OTP message
-                          <Typography variant="body2" color="textPrimary" sx={{ mt: 2 }}>
+                          <Typography
+                            variant="body2"
+                            color="textPrimary"
+                            sx={{ mt: 2 }}
+                          >
                             {otpMessage}
                           </Typography>
                         )}
-                        <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }} onClick={handleSubmitNum}>
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          sx={{ mt: 2 }}
+                          onClick={handleSubmitNum}
+                        >
                           GET OTP
                         </Button>
                       </Box>
@@ -142,11 +216,21 @@ const ForgotPassword = () => {
                           inputProps={{ maxLength: 6 }}
                         />
                         {otpExpiration > 0 && (
-                          <Typography variant="body2" color="textSecondary" sx={{ mt: 2 }}>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            sx={{ mt: 2 }}
+                          >
                             Resend OTP in: {formatTime(otpExpiration)} sec
                           </Typography>
                         )}
-                        <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          color="primary"
+                          fullWidth
+                          sx={{ mt: 2 }}
+                        >
                           Confirm and Continue
                         </Button>
                       </Box>
@@ -154,8 +238,13 @@ const ForgotPassword = () => {
                   </>
                 ) : (
                   <Box>
-                    <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
-                      Please set a new password to help us keep your account safe
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      sx={{ mb: 2 }}
+                    >
+                      Please set a new password to help us keep your account
+                      safe
                     </Typography>
                     <TextField
                       fullWidth
@@ -168,8 +257,15 @@ const ForgotPassword = () => {
                       InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
-                            <IconButton onClick={toggleNewPasswordVisibility} edge="end">
-                              {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                            <IconButton
+                              onClick={toggleNewPasswordVisibility}
+                              edge="end"
+                            >
+                              {showNewPassword ? (
+                                <VisibilityOff />
+                              ) : (
+                                <Visibility />
+                              )}
                             </IconButton>
                           </InputAdornment>
                         ),
@@ -186,14 +282,27 @@ const ForgotPassword = () => {
                       InputProps={{
                         endAdornment: (
                           <InputAdornment position="end">
-                            <IconButton onClick={toggleConfirmPasswordVisibility} edge="end">
-                              {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                            <IconButton
+                              onClick={toggleConfirmPasswordVisibility}
+                              edge="end"
+                            >
+                              {showConfirmPassword ? (
+                                <VisibilityOff />
+                              ) : (
+                                <Visibility />
+                              )}
                             </IconButton>
                           </InputAdornment>
                         ),
                       }}
                     />
-                    <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                      sx={{ mt: 2 }}
+                    >
                       Set Password
                     </Button>
                   </Box>
